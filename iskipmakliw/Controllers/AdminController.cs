a4c84091574807f3f8f40deab5329b05ea94cd22
shintech-asp <sheenrusselcastillo@gmail.com>
2025-09-03 21:02:12 +0800

    Applying Cork UI

---
+++ iskipmakliw/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iskipmakliw.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Sellers()
+        {
+            return View();
+        }
+    }
+}
---
+++ iskipmakliw/Controllers/IndexController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iskipmakliw.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
